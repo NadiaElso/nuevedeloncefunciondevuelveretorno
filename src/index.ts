f3044d74aeb8931d3b58c970557b8fe47beddf3d
NadiaElso <92063948+NadiaElso@users.noreply.github.com>
2021-11-09 19:55:57 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,38 @@
+//por ejemplo si invocamos a la función con los parámetros 3 y a, 
+//el resultado debería ser(3,a)===>[a,a,a]
+
+
+
+function retorno(cantidad:number, valores:any) any {
+
+
+let retornoarreglo:any[]=new Array (1);
+
+
+
+ 
+  for (let i = 0; i <retornoarreglo.length; i++) {
+ 
+  
+  
+retornoarreglo[i]=(cantidad^(valores));
+
+  console.log(retornoarreglo);
+
+
+}
+
+   
+    
+  }
+  return retornoarreglo
+ 
+
+   
+
+
+retorno(4,"a");
+
+
+
+
